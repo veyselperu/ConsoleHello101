823f9db0415581c035c6dda4746426fec78b1843
Waysel Peru <veyselperu@gmail.com>
2021-12-14 00:41:11 +0300

    101

---
+++ ConsoleHello101/ConsoleHello101/ConsoleHello101/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHello101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConsoleHello101 uyarısı.");
+        }
+    }
+}
