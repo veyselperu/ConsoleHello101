cca6b4ad834e099d1ce40aa071be74cbc8ee5ca3
Waysel Peru <veyselperu@gmail.com>
2021-12-13 23:16:19 +0300

    First comit

---
+++ ConsoleHello/ConsoleHello/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            Console.WriteLine($"What\'s your name?");
+            name = Console.ReadLine();
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"Hello {name}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
